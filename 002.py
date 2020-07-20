7ba0282a16b87b348eefef161bd4ced3b92a32fb
HeejaeSeo <chungyounglove@gmail.com>
2020-07-20 14:19:48 +0900

    python 001-006

---
+++ 002.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+## 002. Variable
+
+import math
+r = 3
+pi = math.pi
+area = r ** 2 * pi
+
+print("AREA : ", area)
---
+++ 006.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+## 006. for
+
+result = 0
+for i in range(1, 11) :
+    result += i
+
+print("SUM (1 ~ 10) :", result)
